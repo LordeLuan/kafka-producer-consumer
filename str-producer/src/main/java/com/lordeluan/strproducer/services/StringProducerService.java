66f51e173cbf7a9767d2d5c92b4e6fc922e82386
Luan Lima <lordellu@hotmail.com>
2023-07-26 22:48:17 -0300

    criado projeto para consumir as mensagens do kafka e ja consumindo mensagem enviada

--- str-producer/src/main/java/com/lordeluan/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/lordeluan/strproducer/services/StringProducerService.java
@@ -13,18 +13,20 @@ public class StringProducerService {
 
 	private final KafkaTemplate<String, String> kafkaTemplate;
 	
-//	Enviando mensagem para o kafka e logando em caso de sucesso e ou falha
+//	Enviando mensagem para o kafka no topico str-topic e logando em caso de sucesso e ou falha
 	public void sendMessage(String message) {
-		kafkaTemplate.send("str-topic", message).addCallback(
-			success -> {
-				if(success != null) {
-					log.info("Send message with success {}", message);
-					log.info("Partition {}, offSet {}", 
-								success.getRecordMetadata().partition(),
-								success.getRecordMetadata().offset());
-				}
-			},
-			error -> log.error("Error send menssage")
-		);
+		log.info("send message {}", message);
+		kafkaTemplate.send("str-topic", message);
+//			.addCallback(
+//				success -> {
+//					if(success != null) {
+//						log.info("Send message with success {}", message);
+//						log.info("Partition {}, offSet {}", 
+//									success.getRecordMetadata().partition(),
+//									success.getRecordMetadata().offset());
+//					}
+//				},
+//				error -> log.error("Error send menssage")
+//			);
 	}
 }
