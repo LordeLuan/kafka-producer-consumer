99d64e2de4d78773d474b36627e6ed4d6f1ec6b7
Luan Lima <lordellu@hotmail.com>
2023-08-01 22:00:15 -0300

    adicionado configuracao para produzir mensagens com objeto json

--- payment-service/src/main/java/com/lordenalu/paymentservice/config/JsonProducerConfig.java
+++ payment-service/src/main/java/com/lordenalu/paymentservice/config/JsonProducerConfig.java
@@ -1,5 +1,6 @@
 package com.lordenalu.paymentservice.config;
 
+import java.io.Serializable;
 import java.util.HashMap;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -10,29 +11,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
-public class StringProducerFactoryConfig {
+public class JsonProducerConfig {
 
+//	Pega as informações do Kafka no arquivo properties
 	private final KafkaProperties properties;
 
 //	Cria conexão com o kafka cluster e seta as classes que irão fazer a descerialização das mensagens enviadas
 	@Bean
-	public ProducerFactory<String, String> producerFactory() {
+	public ProducerFactory jsonProducerFactory() {
 		var configs = new HashMap<String, Object>();
 		configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
 		configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		return new DefaultKafkaProducerFactory<>(configs);
+		configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		return new DefaultKafkaProducerFactory<>(configs, new StringSerializer(), new JsonSerializer<>());
 	}
-	
+
 //	O Spring fica responsável por chamar o metodo passado como parametro
 	@Bean
-	public KafkaTemplate<String,String> kafkaTemplate(ProducerFactory<String, String> producerFactory){
-		return new KafkaTemplate<>(producerFactory);
+	public KafkaTemplate<String, Serializable> kafkaTemplate(ProducerFactory jsonProducerFactory) {
+		return new KafkaTemplate<>(jsonProducerFactory);
 	}
 
 }
